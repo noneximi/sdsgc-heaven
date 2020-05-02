d3557adda930a0b5798e16c08f3273e158dfca75
noneximi <34975451+noneximi@users.noreply.github.com>
2020-05-02 18:44:58 -0400

    guild boss page
    added script for guild boss page buttons

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ function generalguideshow(id) {
   }
   document.getElementById(id).style.display = 'block';
 }
+
+function gbguideshow(id) {
+  var arrayOfElements = document.getElementsByClassName('gbbox');
+  var lengthOfArray = arrayOfElements.length;
+
+  for (var i=0; i<lengthOfArray;i++){
+    arrayOfElements[i].style.display='none';
+  }
+  document.getElementById(id).style.display = 'block';
+}
