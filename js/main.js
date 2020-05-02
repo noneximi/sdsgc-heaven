5870ae7a6b85f01ef726338378ad5da253b58865
noneximi <34975451+noneximi@users.noreply.github.com>
2020-05-02 17:22:46 -0400

    setting up guide blocks

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function (){
   $("#header").load("https://noneximi.github.io/sdsgc-heaven/header.html");
 });
+
+function guideshow(a) {
+  var arrayOfElements = document.getElementsByClassName('gtipsbox');
+  var lengthOfArray = arrayOfElements.length;
+
+  for (var i=0; i<lengthOfArray;i++){
+    arrayOfElements[i].style.display='none';
+  }
+  document.getElementById(a).style.display = 'block';
+}
