a0e14a1d9de2ee0e7f5e91031295bdda51155501
noneximi <34975451+noneximi@users.noreply.github.com>
2020-05-03 01:27:17 -0400

    separate header to another file

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+$(function (){
+  $("#mainheader").load("https://noneximi.github.io/sdsgc-heaven/mainheader.html");
+});
+
 (function($) {
 
 	var	$window = $(window),
