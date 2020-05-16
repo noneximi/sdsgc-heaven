d0edfc3a1c0a50bd16337e26899826d7c0a29775
noneximi <34975451+noneximi@users.noreply.github.com>
2020-05-16 15:31:23 -0400

    Create php_sqlserv.php

---
+++ php_sqlserv.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$servername = "pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "h4h3e3qtsw5tyb6s";
+$password = "kwrzwwxn23vj4trr";
+$dbname = "sdsgc-heaven-db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+</body>
+</html>
