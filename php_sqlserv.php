338f84461d744ebffd542b226610a42122405902
noneximi <34975451+noneximi@users.noreply.github.com>
2020-05-16 15:43:21 -0400

    Update php_sqlserv.php

--- php_sqlserv.php
+++ php_sqlserv.php
@@ -7,14 +7,14 @@ $username = "h4h3e3qtsw5tyb6s";
 $password = "kwrzwwxn23vj4trr";
 $dbname = "sdsgc-heaven-db";
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
 ?>
 </body>
 </html>
