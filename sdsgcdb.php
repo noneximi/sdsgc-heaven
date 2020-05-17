8d3f3f66bc26af4b60a09fe78a6fc5963b05955e
noneximi <34975451+noneximi@users.noreply.github.com>
2020-05-17 18:42:31 -0400

    Update sdsgcdb.php

--- sdsgcdb.php
+++ sdsgcdb.php
@@ -72,7 +72,7 @@
 						 <h1>SDSGC Heaven Database (WIP)</h1>
 						 <h4>Sort All SDSGC Characters' Base Stats and Ratings</h4>
 						 <p>Max Stats Data is ungeared (not all character data is available - some new JP releases)</p>
-						 <p>Data sources: <a href="https://docs.google.com/spreadsheets/d/1LPWgA2gJWagYyDwtXKbzQ0G5xuZ0Q4_p2Hvrs01fuM4/">Google Sheets</a>, <a href="https://gcdatabase.com/index.html">GCDatabase</a></p>
+						 <p>Data sources: <a href="https://docs.google.com/spreadsheets/d/1LPWgA2gJWagYyDwtXKbzQ0G5xuZ0Q4_p2Hvrs01fuM4/" target=_blank>Google Sheets</a>, <a href="https://gcdatabase.com/index.html" target=_blank>GCDatabase</a></p>
 					 </div>
 				 </div>
 
