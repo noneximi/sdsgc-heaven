762ed30cb63e4ccad8ebe3c9dc845166914d732f
noneximi <34975451+noneximi@users.noreply.github.com>
2020-05-19 04:08:38 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,6 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-var charactersArr = ["atest", "ctest", "btest"];
-autocomplete(document.getElementById("charSearch"), charactersArr);
 
 $(function (){
   $("#mainheader").load("https://noneximi.github.io/sdsgc-heaven/mainheader.html");
