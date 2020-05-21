661a879d44cc4d0c9819e4743f3f08a7e3c4eb29
noneximi <34975451+noneximi@users.noreply.github.com>
2020-05-21 00:54:37 -0400

    added info

--- sdsgcdb2.php
+++ sdsgcdb2.php
@@ -86,7 +86,7 @@
 						 <h1>SDSGC Character Ratings</h1>
 						 <h4>Sort All SDSGC Characters' Ratings (Click Character Images for Info)</h4>
 						 <h4>Note: Max Stats Data is [Ungeared] [Lvl 80] [6* Awakened] [Ultimate Lvl 1] [No Sacred Treasures]</h4>
-						 <p>Data sources: <a href="https://docs.google.com/spreadsheets/d/1LPWgA2gJWagYyDwtXKbzQ0G5xuZ0Q4_p2Hvrs01fuM4/" target=_blank>Google Sheets</a>, <a href="https://gcdatabase.com/index.html" target=_blank>GCDatabase</a></p>
+						 <p>Data sources: <a href="https://docs.google.com/spreadsheets/d/1LPWgA2gJWagYyDwtXKbzQ0G5xuZ0Q4_p2Hvrs01fuM4/" target=_blank>Google Sheets</a>, <a href="https://gcdatabase.com/index.html" target=_blank>GCDatabase</a>, <a href="https://gamewith.jp/7taizai/article/show/158813" target=_blank>GameWith jp</a></p>
 					 </div>
 				 </div>
 
