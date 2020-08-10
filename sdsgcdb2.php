7ac0fe34697a9d6078cc620566c03cd206aee0fd
noneximi <34975451+noneximi@users.noreply.github.com>
2020-08-10 14:02:33 -0400

    test pvp table

--- sdsgcdb2.php
+++ sdsgcdb2.php
@@ -157,12 +157,12 @@
 						$result = $conn->query($sql);
 
 						if ($result->num_rows > 0) {
-							echo '<table>';
+							echo '<table><tr>';
 							// output data of each row
 							while($row = $result->fetch_assoc()) {
-							echo '<tr><td></td><td>PVP</td><td>Farming</td><td>Passive</td></tr><tr><td><a onclick="modalFunction(\'' . $row["CharacterName"] . 'Modal\')"><span class="image"><img src="' .  $row["ImageLink"] . '" alt="" /></span></a></td><td>' . $row["PVP"] . '</td><td>'. $row["Farming"]. '</td><td>' . $row["Passive"] . '</td></tr>';
+							echo '<td></td><td>PVP</td><td>Farming</td><td>Passive</td></tr><tr><td><a onclick="modalFunction(\'' . $row["CharacterName"] . 'Modal\')"><span class="image"><img src="' .  $row["ImageLink"] . '" alt="" /></span></a></td><td>' . $row["PVP"] . '</td><td>'. $row["Farming"]. '</td><td>' . $row["Passive"] . '</td>';
 							}
-							echo '</table>';
+							echo '</tr></table>';
 						} else {
 							echo "0 results";
 						}
