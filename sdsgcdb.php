00cd4adfc71faa8e358cf7305c6b30d58a6afffc
noneximi <34975451+noneximi@users.noreply.github.com>
2020-08-10 17:23:23 -0400

    better labelling

--- sdsgcdb.php
+++ sdsgcdb.php
@@ -82,8 +82,8 @@
 
 					<div class="content">
 					 <div class="inner">
-						 <h1>SDSGC Character Stats</h1>
-						 <h4>Sort All SDSGC Characters' Stats (Click Character Images for Info)</h4>
+						 <h1>Character Stats</h1>
+						 <h4>Sort All Characters' Stats (Click Character Images for Info)</h4>
 						 <h4>Note: Max Stats Data is [Ungeared] [Lvl 80] [6* Awakened] [Ultimate Lvl 1] [No Sacred Treasures]</h4>
 						 <p>Data sources: <a href="https://docs.google.com/spreadsheets/d/1LPWgA2gJWagYyDwtXKbzQ0G5xuZ0Q4_p2Hvrs01fuM4/" target=_blank>Google Sheets</a>, <a href="https://gcdatabase.com/index.html" target=_blank>GCDatabase</a>, <a href="https://gamewith.jp/7taizai/article/show/158813" target=_blank>GameWith jp</a></p>
 					 </div>
